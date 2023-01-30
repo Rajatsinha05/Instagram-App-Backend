a240df66f948e8b0a15fea2eb45dc07a28a2b1c2
Rajatsinha05 <rajatsinha5467@gmail.com>
2023-01-30 21:31:15 +0530

    check

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const connect = require('./config/connect')
 const routes  = require('./routes/routes')
 
 const cors = require('cors')
-const PORT = process.env.PORT || 8081;
+
 const app = express()
 app.use(cors())
 app.use(express.json())
+const port = process.env.PORT || 8081;
 
 
 app.use('/',routes)
-app.listen(PORT,()=>{
+app.listen(port,()=>{
 
     console.log("server working port 8081")
 })
