4cbd65355e56e66aae5777c638dfa21d16705622
Rajatsinha05 <rajatsinha5467@gmail.com>
2023-01-30 20:49:41 +0530

    user model

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const connect = require('./config/connect')
-const { route } = require('./routes/routes')
+const routes  = require('./routes/routes')
 
 
 
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 
 
-app.use('/',route)
+app.use('/',routes)
 app.listen(8081,()=>{
 
     console.log("server working port 8081")
--- models/User.Schema.js
+++ models/User.Schema.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-  name: { type: string, required: true },
-  email: { type: string, required: true },
-  gender: { type: string, required: true },
-  password: { type: string, required: true },
-  posts: Array,
-});
+// const mongoose = require("mongoose");
+// const userSchema = new mongoose.Schema({
+//   name: { type: string, required: true },
+//   email: { type: string, required: true },
+//   gender: { type: string, required: true },
+//   password: { type: string, required: true },
+//   posts: Array,
+// });
 
-const User = mongoose.model("User", userSchema);
+// const User = mongoose.model("User", userSchema);
 
-module.exports = User;
+// module.exports = User;
