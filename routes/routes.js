1c30e393a51c0f8c7ca840a91bb6e12799c9b863
Rajatsinha05 <rajatsinha5467@gmail.com>
2023-01-30 20:59:16 +0530

    user register route

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
 const {Router} =require('express')
+const User = require('../models/User.Schema')
 
 
 const routes =Router()
@@ -9,4 +10,15 @@ routes.get('/',(req,res) => {
     res.send("routes")
 })
 
+
+routes.post('/users/register',async(req,res) => {
+
+
+
+   let data = await User.create(req.body)
+   res.send(data)
+})
+
+
 module.exports=routes
+
